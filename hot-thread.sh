56161a516c8462d7947fc1310d1c8aa3a60b5c38
Giles Westwood <Giles.Westwood@semantico.com>
2019-02-21 12:38:23 +0000

    master: typo

--- hot-thread.sh
+++ hot-thread.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-${JAVA_HOME}/bin/java -Xbootclasspath/a:${JAVA_HOME}/lib/tools.jar -jar HotThreads.jar $1
+${JAVA_HOME}/bin/java -Xbootclasspath/a:${JAVA_HOME}/lib/tools.jar -jar HotThread.jar $1
